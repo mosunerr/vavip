258a300c4daed695b05679542991a3e34fd0625f
MacBook <macbook@MacBook-Pro-MacBook.local>
2025-09-09 22:20:17 +0300

    Рабочий вариант главной страницы

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def compare():
     return render_template('compare.html', units=selected_units)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5002)
 
