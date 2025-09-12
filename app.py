c7bde2570a198b83c513cea1cc9f5cc9bf503e78
MacBook <macbook@MacBook-Pro-MacBook.local>
2025-09-12 23:16:11 +0300

    Обновил в футере надпись вавип - вместо текста вставил файл .png; Обновил видео; и много чего еще :)

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def compare():
     return render_template('compare.html', units=selected_units)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
